a7cdfced50f3f87dd4fb9547276669e2735bd573
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-09-27 12:48:31 -0300

    Update basic-feats.js

--- scripts/data/basic-feats.js
+++ scripts/data/basic-feats.js
@@ -26,87 +26,307 @@ export const basic_feats = [
     ],
     [
         "Ambidestro",
-        "A penalidade de ataque com duas armas na mão secundária é igual ao da primária.",
+        "A penalidade de ataque com duas armas na mão secundária é igual ao da mão primária.",
         ["14 de Agilidade"]
     ],
     [
         "Andar Silencioso",
-        "",
+        "+2 em Furtividade.",
         [""]
     ],
     [
         "Ardiloso",
-        "",
+        "+2 em Ladroagem.",
         [""]
     ],
     [
         "Artesão",
-        "",
+        "Você tem treinamento com 1 Ferramenta de Artesão.",
         [""]
     ],
     [
         "Artista",
-        "",
+        "+2 em Atuação.",
         [""]
     ],
     [
         "Ataque Cauteloso",
-        "",
-        [""]
+        "Você faz uma ação de ataque com -2 na rolagem, porém recebe +2 na Defesa, até o próximo turno.",
+        ["13 de Sabedoria"]
     ],
     [
         "Ataque Poderoso",
-        "",
-        [""]
+        "Antes de uma Rolagem de Ataque você pode escolher um número para subtrair do ataque e adicionar ao dano.",
+        ["13 de Força"]
     ],
     [
         "Atencioso",
-        "",
+        "+2 em Percepção.",
         [""]
     ],
     [
         "Atirador Aguçado",
-        "",
+        "Você pode atirar em um alvo a até 5 m depois do limite de alcance de sua arma a distância, porém com uma penalidade de -4 na rolagem de ataque e -2 no dano.",
         [""]
     ],
     [
         "Atletismo",
-        "",
+        "+2 em Atletismo.",
         ""
     ],
     [
         "Bater com o Escudo",
-        "",
-        [""]
+        "Com uma Ação Menor após a manobra de combate Encontrão, você pode executar um ataque com o escudo.",
+        ["Treinamento com Escudo"]
     ],
     [
         "Capacidade Mental",
-        "",
+        "+4 de Mana. <b>Acumulável até 3x.</b>",
         [""]
     ],
     [
         "Carismático",
-        "",
-        [""]
+        "+2 em Diplomacia.",
+        ["12 de Carisma"]
     ],
     [
         "Combate às Cegas",
-        "",
+        "Você tem 2 m de Visão às Cegas.",
         [""]
     ],
     [
         "Combate com Armas Grandes",
-        "",
-        [""]
+        "Você pode utilizar uma arma de duas mãos em uma mão, desde que a arma não seja a distância. Se você usar uma arma de duas mãos, com as duas mãos, seu dado de dano aumenta em uma escala (d4 -> d6 -> d8 -> d10 -> d12 -> 2d6).",
+        ["14 de Força"]
     ],
     [
         "Combatente Ágil",
-        "",
+        "Quando você fizer um acerto crítico com um ataque corpo a corpo, você pode se deslocar 1 m como uma ação livre.",
+        ["16 de Agilidade"]
+    ],
+    [
+        "Conjurador de Batalha",
+        "Você ignora 20% de Falha Arcana total entre Armaduras e Escudos. <b>Acumulável até 3x.</b>",
+        ["Treinamento com Armaduras Médias ou Pesadas"]
+    ],
+    [
+        "Corajoso",
+        "+2 em Coragem.",
         [""]
     ],
     [
-        "",
-        "",
+        "Crítico Poderoso",
+        "+1 em Danos Críticos. <b>Acumulável até 3x.</b>",
         [""]
-    ]
+    ],
+    [
+        "Curandeiro",
+        "Treinamento com Kit de Herbalista.",
+        [""]
+    ],
+    [
+        "Desviar Projétil",
+        "Uma vez a cada Descanso, como uma Reação, você pode ignorar 1d10 + seu modificador de Agilidade, do dano proveniente de armas a distância.",
+        ["14 de Agilidade"]
+    ],
+    [
+        "Duro de Matar",
+        "A cada Descanso Longo, na primeira vez que um dano deixe você com 0 ou menos pontos de vida, você fica com 1 de vida.",
+        ["<span class='kw'>Físico Preparado</span>"]
+    ],
+    [
+        "Enfermeiro",
+        "+2 em Medicina.",
+        [""]
+    ],
+    [
+        "Engenhoso",
+        "+2 em Construir.",
+        [""]
+    ],
+    [
+        "Enrolador",
+        "+2 em Enganação.",
+        [""]
+    ],
+    [
+        "Esquiva",
+        "+1 em Defesa.",
+        ["14 de Agilidade"]
+    ],
+    [
+        "Esquiva Acrobática",
+        "Quando você for atingido por um Ataque de Oportunidade, você pode executar um teste de Acrobacia com dificuldade igual a Rolagem do Ataque, em caso de sucesso você evita o ataque.",
+        ["<span class='kw'>Esquiva</span>"]
+    ],
+    [
+        "Estudado",
+        "+2 em Conhecimento.",
+        [""]
+    ],
+    [
+        "Estudioso Arcano",
+        "+2 em Arcanismo.",
+        [""]
+    ],
+    [
+        "Estudioso Espiritual",
+        "+2 em Religião.",
+        [""]
+    ],
+    [
+        "Explorador de Masmorra",
+        "Vantagem nos testes de Percepção e Investigação quando estiver em Masmorras.",
+        [""]
+    ],
+    [
+        "Intimidador",
+        "+2 em Intimidação.",
+        [""]
+    ],
+    [
+        "Investigador",
+        "+2 em Investigação.",
+        [""]
+    ],
+    [
+        "Líder Inspirador",
+        "Durante o primeiro turno de um combate, você e seus aliados tem um bônus nas Rolagem de Ataque, igual ao seu modificador de Carisma.",
+        ["14 de Carisma"]
+    ],
+    [
+        "Lutador",
+        "Seu dado de dano dos Ataques Desarmados é um d4.",
+        ["14 de Força"]
+    ],
+    [
+        "Magia de Reação",
+        "Como uma Reação a um ataque corpo a corpo que o atingir, você pode conjurar uma magia com alcance de Toque no atacante.",
+        [""]
+    ],
+    [
+        "Magia Rápida",
+        "Uma vez a cada Descanso, você pode conjurar uma magia que tenha duração de uma Ação, como uma Ação Menor.",
+        [""]
+    ],
+    [
+        "Meditação",
+        "Uma vez por dia, durante um Descanso Curto você faz uma meditação que recupera vida ou mana, a sua escolha, igual ao dobro do seu modificador de Concentração.",
+        [""]
+    ],
+    [
+        "Mente Focada",
+        "+2 em Concentração.",
+        [""]
+    ],
+    [
+        "Mestre das Armas",
+        "Escolha 4 armas para ser treinado.",
+        [""]
+    ],
+    [
+        "Mestre de Armas Exóticas",
+        "Escolha 2 Armas Exóticas para ser treinado.",
+        ["<span class='kw'>Mestre das Armas</span>"]
+    ],
+    [
+        "Mobilidade",
+        "Você tem +2 na Defesa quando sofrer um Ataque de Oportunidade.",
+        ["<span class='kw'>Esquiva</span>"]
+    ],
+    [
+        "Músico",
+        "Você tem treinamento em 2 Instrumentos Musicais.",
+        [""]
+    ],
+    [
+        "Ódio",
+        "Escolha um tipo de criatura, (Se escolher Humanoide, escolha 3 raças deste tipo), quando causar dano a este tipo escolhido, você causa 1d4 de dano adicional.",
+        [""]
+    ],
+    [
+        "Oportunidade",
+        "+2 na rolagem de um Ataque de Oportunidade.",
+        [""]
+    ],
+    [
+        "Poliglota",
+        "Você sabe falar 2 Idiomas adicionais.",
+        [""]
+    ],
+    [
+        "Proficiência com Arma",
+        "+1 nas Rolagens de Ataque de uma arma específica.",
+        [""]
+    ],
+    [
+        "Prontidão",
+        "+2 em Iniciativa.",
+        [""]
+    ],
+    [
+        "Proteção Psíquica",
+        "+2 em Testes de Salvamento envolvendo dano ou efeitos Psíquicos.",
+        ["12 de Vontade"]
+    ],
+    [
+        "Punhos de Ferro",
+        "+1 no Dano de Ataques Desarmados.",
+        ["12 de Força"]
+    ],
+    [
+        "Recarga Veloz",
+        "Armas com a característica de Recarga necessitam de apenas uma Ação Menor para a recarga.",
+        [""]
+    ],
+    [
+        "Resistente",
+        "Uma vez a cada Descanso Longo, quando você ficar com menos da metade da vida máxima, você pode se curar em um valor igual a seu modificador de Vitalidade.",
+        ["14 de Vitalidade"]
+    ],
+    [
+        "Robusto",
+        "+2 Pontos de Vida.",
+        [""]
+    ],
+    [
+        "Saque Rápido",
+        "Você consegue sacar armas de uma mão com uma Ação Menor.",
+        ["12 de Agilidade"]
+    ],
+    [
+        "Silvestre",
+        "+2 em Sobrevivência.",
+        [""]
+    ],
+    [
+        "Sortudo",
+        "Uma vez a cada Descanso, em uma Rolagem de d20 com resultado igual a 1, você pode rolar novamente o teste.",
+        [""]
+    ],
+    [
+        "Talentoso",
+        "+1 no Bônus de um Talento Básico ou Heroico. (Defina com o mestre o efeito).",
+        [""]
+    ],
+    [
+        "Táticas de Emboscada",
+        "Você causa 1d6 de dano adicional contra inimigos que ainda não agiram no combate.",
+        ["12 de Sabedoria"]
+    ],
+    [
+        "Valentia",
+        "+1 no Dano em ataques corpo a corpo.",
+        [""]
+    ],
+    [
+        "Vantagem Agressiva",
+        "Você tem Vantagem no primeiro ataque do combate.",
+        [""]
+    ],
+    [
+        "Vigor",
+        "+2 em Resistência.",
+        [""]
+    ],
 ]
