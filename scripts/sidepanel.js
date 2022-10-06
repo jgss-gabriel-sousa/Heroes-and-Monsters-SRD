a26a47475ad71f6381bafae773030c0fcff15a64
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-10-06 11:16:54 -0300

    sidepanel improvement

--- scripts/sidepanel.js
+++ scripts/sidepanel.js
@@ -1,6 +1,7 @@
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
     document.getElementById("mySidepanel").style.height = "max-content";
+    document.getElementById("main-interface").style.marginLeft = "300px";
     document.querySelector(".openbtn").style.display = "none";
 }
 
@@ -8,4 +9,5 @@ function closeNav() {
     document.querySelector(".openbtn").style.display = "inline-block";
     document.getElementById("mySidepanel").style.width = "0";
     document.getElementById("mySidepanel").style.height = "0";
+    document.getElementById("main-interface").style.marginLeft = "0";
 }
